02be74e46edd3271041b0dcf99dd05a45a2a6759
Garvit Gupta <garvit@seas.upenn.edu>
2017-12-05 12:20:26 -0500

    Added loader

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,40 +1,44 @@
 var app = angular.module('directions', []);
-app.controller('directionsController', function($scope, $http) {
+app.controller('directionsController', function($scope, $http, $timeout) {
 
 	$scope.search = function() {
 
 		// Show the loader
 		$scope.show = true;
 
-		var data = get_best_path($scope.source,$scope.destination);
-		jsonMake = data;
+		$timeout(function() {
 
-		var row = "[";
+			var data = get_best_path($scope.source,$scope.destination);
+			jsonMake = data;
 
-		for (var i=0; i<jsonMake[2].length; i++) {
+			var row = "[";
 
-			if (jsonMake[2][i][2].substring(0,3) == "WAL") {
-				jsonMake[2][i][2] = "mode-icon-walk";
+			for (var i=0; i<jsonMake[2].length; i++) {
+
+				if (jsonMake[2][i][2].substring(0,3) == "WAL") {
+					jsonMake[2][i][2] = "mode-icon-walk";
+				}
+
+				else if (jsonMake[2][i][2].substring(0,3) == "BIK") {
+					jsonMake[2][i][2] = "mode-icon-bike";
+				}
+
+				else if (jsonMake[2][i][2].substring(0,3) == "SUB") {
+					jsonMake[2][i][2] = "mode-icon-subway";
+				}
+
+				var row = row + "{\"start\": \"" +  jsonMake[2][i][0] + "\", \"end\": \"" + jsonMake[2][i][1] + "\", \"mode\": \"" + jsonMake[2][i][2] + "\", \"time\":" + jsonMake[2][i][3].toFixed(2) + ", \"distance\":" + jsonMake[2][i][4].toFixed(2) + "},";
 			}
 
-			else if (jsonMake[2][i][2].substring(0,3) == "BIK") {
-				jsonMake[2][i][2] = "mode-icon-bike";
-			}
+			row = row.substring(0, row.length-1);
+			var row = row + "]";
 
-			else if (jsonMake[2][i][2].substring(0,3) == "SUB") {
-				jsonMake[2][i][2] = "mode-icon-subway";
-			}
+			jsonObject = JSON.parse(row);
+			$scope.path = jsonObject;
+			$scope.totalTime = jsonMake[1].toFixed(2);
+			$scope.show = false;
 
-			var row = row + "{\"start\": \"" +  jsonMake[2][i][0] + "\", \"end\": \"" + jsonMake[2][i][1] + "\", \"mode\": \"" + jsonMake[2][i][2] + "\", \"time\":" + jsonMake[2][i][3].toFixed(2) + ", \"distance\":" + jsonMake[2][i][4].toFixed(2) + "},";
-		}
-
-		row = row.substring(0, row.length-1);
-		var row = row + "]";
-
-		jsonObject = JSON.parse(row);
-		$scope.path = jsonObject;
-		$scope.totalTime = jsonMake[1].toFixed(2);
-		$scope.show = false;
+		}, 500);
 	}
 	/* Set Defaults */
 
